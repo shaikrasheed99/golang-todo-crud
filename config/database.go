d7285a3338f06f12d8a4540f4dd51bcb83a7640d
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-05-22 15:49:25 +0530

    Add. Database config

---
+++ config/database.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"todo-crud/helper"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbName   = "todos"
+)
+
+func DatabaseConnection() *gorm.DB {
+	connectionString := fmt.Sprintf("host=%s port%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+
+	helper.LogAndPanicError(err)
+
+	return db
+}
